f98f3481927c647e9bb1cf90ebe3688bdc7100af
Vinícius H <vini2003henri12395@gmail.com>
2023-05-28 21:10:16 -0300

    Iniciando conexão com o servidor

--- frontend/src/app/components/cadastro/formCadastro.tsx
+++ frontend/src/app/components/cadastro/formCadastro.tsx
@@ -1,26 +1,72 @@
 import Link from 'next/link'
 import * as S from './style'
+import { string, z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createdData } from '@/app/types/typesUser'
+
+
 
 const CadastroForm = () => {
+    const createUserSchema = z.object({
+        name: string().nonempty('O campo nome é obrigatório')
+        .transform(name => {
+            return name.trim().split(' ').map(word => {
+                return word[0].toLocaleUpperCase().concat(word.substring(1))
+            }).join(' ')
+        }),
+        email: string().nonempty('O campo email é obrigatório')
+        .email('Email invalido').toLowerCase(),
+        password: string().min(6, 'O minimo é 6 caracteres')
+        .nonempty('O campo senha é obrigatório')
+    })
+
+    type CreateUserFormData = z.infer<typeof createUserSchema>
+
+    const { register, handleSubmit, formState: {errors} } = useForm<CreateUserFormData>({
+        resolver: zodResolver(createUserSchema)
+    })
+
+    const createUser = (data: createdData) => {
+        console.log(JSON.stringify(data))
+    }
+
     return(
         <>
-            <S.Form>
+            <S.Form onSubmit={handleSubmit(createUser)}>
                 <S.Title>Cadastrar</S.Title>
                 <S.Div>
-                    <S.Input type='text' placeholder='Digite seu nome...'/>
+                    <S.Input 
+                        type='text' 
+                        placeholder='Digite seu nome...'
+                        {...register('name')}
+                    />
+                </S.Div>
+                {errors.name && <span>{errors.name.message}</span>}
+                <S.Div>
+                    <S.Input 
+                        type='email' 
+                        placeholder='Digite seu e-mail...'
+                        {...register('email')}
+                    />
+                </S.Div>
+                {errors.email && <span>{errors.email.message}</span>}
+                <S.Div>
+                    <S.Input 
+                        type='password' 
+                        placeholder='Digite sua senha...'
+                        {...register('password')}
+                    />
                 </S.Div>
                 <S.Div>
-                    <S.Input type='email' placeholder='Digite seu e-mail...'/>
-                </S.Div>
-                <S.Div>
-                    <S.Input type='password' placeholder='Digite sua senha...'/>
-                </S.Div>
-                <S.Div>
-                    <S.Input type='password' placeholder='Digite sua senha...'/>
+                    <S.Input 
+                        type='password' 
+                        placeholder='Confirme sua senha'
+                        {...register('password')}
+                    />
                 </S.Div>
                 <S.Div>
                     <Link href={"/"}><S.Link>Voltar</S.Link></Link>
-                    
                     <S.Button>Cadastrar</S.Button>
                 </S.Div>
             </S.Form>
--- frontend/src/app/components/login/formLogin.tsx
+++ frontend/src/app/components/login/formLogin.tsx
@@ -1,31 +1,54 @@
 import Link from 'next/link';
 import * as S from './style';
-import { useState } from 'react';
+import { string, z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {loginData} from '../../types/typesUser';
+import baseUrl from '@/app/api/_api'
+
 
 const FormLogin = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const loginUserSchema = z.object({
+        email: string().nonempty('Preencha o campo email').email('Email invalido!').toLowerCase(),
+        password: string().min(6, 'Minimo 6 caracteres').nonempty('O campo senha é obrigatório')
+    })
 
+    type LoginUserData = z.infer<typeof loginUserSchema>
+
+    const {register, handleSubmit, formState: {errors}} = useForm<LoginUserData>({
+        resolver: zodResolver(loginUserSchema)
+    })
+
+    const loginUser = async (data: loginData) => {
+        try {
+            const response = await baseUrl.post('/login', data)
+            const dataApi = await response.data
+            console.log(dataApi)
+            return dataApi
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return(
-        <S.Form>
+        <S.Form onSubmit={handleSubmit(loginUser)}>
             <S.Title>Login</S.Title>
             <S.Div>
                <S.Input 
                     type='email' 
                     placeholder='Digite seu email...'
-                    onChange={(event) => setEmail(event.target.value)}
-                    value={email}
+                    {...register('email')}
                /> 
             </S.Div>
+            {errors.email && <span>{errors.email.message}</span>}
             <S.Div>
                <S.Input 
                     type='password' 
                     placeholder='Digite sua senha...'
-                    onChange={(event) => setPassword(event.target.value)}
-                    value={password}
+                    {...register('password')}
                /> 
             </S.Div>
+            {errors.password && <span>{errors.password.message}</span>}
             <S.Div>
                 <Link href={"/cadastro"}>
                     <S.Link>Cadastre-se</S.Link>
